fbd7412281257b5867a736e9c701992e4e1ea640
Dtpark <dtpark@ghtt.net>
2022-05-20 14:42:26 +0800

    compelet the main.go of user

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
-	"log"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/limit"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	etcd "github.com/kitex-contrib/registry-etcd"
+	trace "github.com/kitex-contrib/tracer-opentracing"
+	"micro_tiktok/cmd/user/dal"
 	user "micro_tiktok/kitex_gen/user/userservice"
+	"micro_tiktok/pkg/constants"
+	tracer2 "micro_tiktok/pkg/tracer"
+	"net"
 )
 
+func Init() {
+	tracer2.InitJaeger(constants.UserServiceName)
+	dal.Init()
+}
+
 func main() {
-	svr := user.NewServer(new(UserServiceImpl))
-
-	err := svr.Run()
-
+	r, err := etcd.NewEtcdRegistry([]string{constants.EtcdAddress})
 	if err != nil {
-		log.Println(err.Error())
+		panic(err)
+	}
+
+	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:18080")
+	if err != nil {
+		panic(err)
+	}
+
+	Init()
+
+	svr := user.NewServer(new(UserServiceImpl),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: constants.UserServiceName}), // server name
+		server.WithServiceAddr(addr),                                       // address
+		server.WithLimit(&limit.Option{MaxConnections: 1000, MaxQPS: 100}), // limit
+		server.WithMuxTransport(),                                          // Multiplex
+		server.WithSuite(trace.NewDefaultServerSuite()),                    // tracer
+		server.WithRegistry(r),                                             // registry
+	)
+
+	err = svr.Run()
+	if err != nil {
+		klog.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,9 @@ module micro_tiktok
 go 1.16
 
 require (
-	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
 	github.com/cloudwego/kitex v0.3.1
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/kitex-contrib/registry-etcd v0.0.0-20220110034026-b1c94979cea3
 	github.com/uber/jaeger-client-go v2.30.0+incompatible
-	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	google.golang.org/protobuf v1.28.0
 	gorm.io/driver/mysql v1.3.3

3e8486d3103cef30561fbef209f0cd761238afae
Dtpark <dtpark@ghtt.net>
2022-05-29 22:05:52 +0800

    add the redis address constant and modify the idl of video

--- pkg/constants/constant.go
+++ pkg/constants/constant.go
@@ -9,5 +9,6 @@ const (
 	JWTSecretKey    = "ByteDanceCamp3"
 	IdentityKey     = "uid"
 	EtcdAddress     = "127.0.0.1:12379"
+	RedisAddress    = "127.0.0.1:16379"
 	MySQLDefaultDSN = "mtt:4&o4csZZ^OMDiy1Q@tcp(localhost:13306)/micro_tiktok?charset=utf8&parseTime=True&loc=Local"
 )
